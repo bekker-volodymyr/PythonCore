b5e6b4ae168701c6afc59b487a48150a136113fc
bekkerVU <bekker.volodymyr.yu@gmail.com>
2026-02-03 11:47:47 +0200

    strings

---
+++ 8_strings.py
@@ -0,0 +1,53 @@
+text = "Hello, world!"
+
+print(text[0]) # H
+print(text[7]) # w
+
+#print(text[22]) # Помилка!
+
+print(text[:10])    # Hello, wor
+print(text[3:6])    # lo,
+print(text[3:])     # lo, world!
+print(text[3:10:4]) # lw
+print(text[::2])    # Hlo ol!
+
+print(text[-1]) # !
+print(text[-4]) # r
+
+print(text[::-1])       # !dlrow ,olleH
+print(text[:-7:-1])     # !dlrow
+print(text[-3:-10:-1])  # lrow ,o
+
+# text[5] = "U" # помилка
+
+print(len(text)) # 13
+
+counter = 0
+while counter < len(text):
+    print(text[counter], end=', ')
+    counter += 1
+
+print()
+
+for i in text:
+    print(i, end=', ')
+
+print()
+
+text_upper = text.upper()
+text_lower = text.lower()
+print(text_upper) # всі великі
+print(text_lower) # всі маленькі
+print(text) # оригінальний рядок не змінився!
+
+text = "   Hello, world!    "
+text_stripped = text.strip()
+print(text_stripped)
+print(text) # Не змінився!
+
+text = text_stripped
+
+text_modified = text.replace('l', 'P')
+text_modified2 = text.replace('world', 'python')
+print(text_modified)
+print(text_modified2)
