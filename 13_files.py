462c7f0f05be1858c1af0e081545e417a7aff837
bekkerVU <bekker.volodymyr.yu@gmail.com>
2026-03-20 10:28:16 +0200

    files

--- 13_files.py
+++ 13_files.py
@@ -1,5 +1,46 @@
 file_path = "test.txt" # шлях до файлу
 
-file = open(file_path) # відкриття файлу в режими +a
+file = open(file_path, '+a') # відкриття файлу в режими +a
 
 file.close() # закриття файлу
+
+with open(file_path, '+a') as f:
+    print("File test.txt was created!")
+
+
+file_path = './txt_files/demofile.txt' # . означає поточний каталог
+
+with open(file_path, 'r') as f:
+    file_text = f.read()
+    print(file_text)
+
+with open(file_path, 'r') as f:
+    file_text = f.read(6)
+    print(file_text)
+
+with open(file_path, 'r') as f:
+    line_1 = f.readline()
+    print(line_1)
+    line_2 = f.readline()
+    print(line_2)
+
+with open(file_path, 'r') as f:
+    lines = f.readlines()
+    print(lines)
+
+with open(file_path, 'r') as f:
+    for line in f:
+        print(line)
+
+with open("demofile.txt", "a") as f:
+    f.write("Now the file has more content!")
+
+with open("demofile.txt", 'w') as f:
+    f.write("Woops! I have deleted the content!")
+
+
+try:
+    with open("demofile.txt", 'x'):
+        print('File was successfully created!')
+except FileExistsError:
+    print('File already exists!')
